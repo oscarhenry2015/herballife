cb832fc37637848fc10466dc1208a8a7d0441904
oscar <84227494+oscarhenry2015@users.noreply.github.com>
2025-03-29 14:14:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import passport from "passport";
 import { Strategy } from "passport-local";
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 const saltRounds = 12;
 // Initialize env
 env.config();
